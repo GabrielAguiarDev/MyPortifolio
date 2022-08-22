37918587cfb8869d30617349da8529a5d072968a
Gabriel Aguiar <gabriel.aguiar.dev@gmail.com>
2022-08-22 20:42:40 -0300

    correção de alguns bugs visuais

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer>
-      <p>Developed by <a href="https://www.linkedin.com/in/gabriel-aguiar-dev/">Gabriel Aguiar</a> <span>❤</span></p>
+      <p>Developed by <a href="https://www.linkedin.com/in/gabriel-aguiar-dev/">Gabriel Aguiar</a> <span>🚩</span></p>
     </footer>
   )
 }
